d27dc73fca7c3438306280d15fcaaa2a8ba63281
Warren Butt <warrenb@iqmetrix.com>
2013-10-31 16:09:00 -0600

    Initial commit of working response endpoint

---
+++ ElectricImpHackathon/Controllers/MusicController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectricImpHackathon.Controllers
+{
+    public class MusicController : Controller
+    {
+        //
+        // GET: /Music/
+
+        public ViewResult Go()
+        {
+            var musicString = Request.Params["body"];
+
+            var request = System.Net.WebRequest.Create("https://agent.electricimp.com/qcd3tqH2yirA?led=" + musicString);
+
+            var result = request.BeginGetResponse(null, null);
+
+            return View();
+        }
+    }
+}
---
+++ ElectricImpHackathon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElectricImpHackathon.MvcApplication" Language="C#" %>
